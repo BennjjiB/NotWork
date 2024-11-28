3e2d78cd4ad5c665511b86f839d8177794365fa6
Benjamin Banzhaf <benni@slab.de>
2024-11-28 10:30:01 +0100

    - raffle ui + responsive

--- mint-ui/pages/raffle.tsx
+++ mint-ui/pages/raffle.tsx
@@ -2,37 +2,32 @@
 import React, {useState, useEffect} from 'react'
 import {useUmi} from "../utils/useUmi"
 import {amountToNumber} from "@metaplex-foundation/umi"
-import {Heading, HStack, Image, Text, VStack} from "@chakra-ui/react"
+import {Heading, HStack, Image, Stack, Text, VStack} from "@chakra-ui/react"
 import {fetchSolBalance, fetchTokenBalance} from "../utils/transactions"
-import NextImage from "next/image"
+import NextImage, {StaticImageData} from "next/image"
 import raffle_title_image from 'public/RaffleTitle.png'
 import prices from 'public/RafflePrices.png'
 import ticket from 'public/RaffleTicket.png'
 import buttonImage from 'public/RaffleBuyBotton.png'
 import {Slider} from "../components/ui/slider"
 import {Table} from "@chakra-ui/react"
-
+import styles from "../styles/Home.module.css"
 
 export default function Raffle() {
   const umi = useUmi()
-  const [solBalance, setSolBalance] = useState(0) // State for Sol balance
   const [notworkBalance, setNotworkBalance] = useState<number>(0) // Ensure it's typed as a number
 
   useEffect(() => {
     (async () => {
       if (!umi) return
-      const solAmount = await fetchSolBalance(umi)
-      if (solAmount) {
-        setSolBalance(amountToNumber(solAmount))
-      }
-
       setNotworkBalance(await fetchTokenBalance(umi) ?? 0)
     })()
   }, [umi])
 
+  const error = umi.payer.publicKey == "11111111111111111111111111111111" || notworkBalance == 0
   const BuyButton = () => {
     return (
-      <VStack>
+      <VStack gap={"0rem"}>
         <Image
           onClick={
             () => (async () => {
@@ -41,27 +36,32 @@ export default function Raffle() {
             })()
           }
           w="100%"
-          h={"6rem"}
+          h={{base: "3rem", sm: "6rem", md: "4rem", lg: "6rem"}}
           fit="contain"
           alt="Title Image"
+          opacity={error ? "0.6" : "1"}
           asChild
-          _active={{transform: "translateY(3px)"}}
+          transition="all .05s ease-in-out"
+          _hover={error ? {} : {filter: "drop-shadow(0 0 2.5rem rgb(255, 233, 0, 0.5))"}}
+          _active={error ? {} : {transform: "translateY(3px)"}}
         >
           <NextImage src={buttonImage} alt={"Referral link image"}/>
         </Image>
         {
-          true ? (<Text color={"#FF0000"}>{"Text"}</Text>) : null
+          (error) ? (
+            <Text color={"#FF0000"}>{
+              (umi.payer.publicKey == "11111111111111111111111111111111") ?
+                "Please connect your wallet to enter the raffle" :
+                "You need $notwork to enter the raffle!"
+            }</Text>) : null
         }
       </VStack>
     )
   }
 
-  function getNotworkAmount(tickets: number): number {
-    return tickets * 1000
-  }
-
   const [counter, setCounter] = React.useState("")
 
+  /*
   useEffect(() => {
     let countDownDate = new Date("Jan 5, 2030 15:37:25").getTime()
 
@@ -81,28 +81,42 @@ export default function Raffle() {
       }
     }, 1000)
   }, [])
+*/
 
+  interface TicketIndicatorProps {
+    tickets: number
+  }
+
+  const TicketIndicator = (props: TicketIndicatorProps) => {
+    return (
+      <HStack alignItems="center">
+        <Heading textAlign="center" textStyle={{base: "4xl", lg: "6xl"}}>
+          {props.tickets}
+        </Heading>
+        <Image w={{base: "10rem", md: "5rem", lg: "10rem"}} h={{base: "10rem", md: "5rem", lg: "10rem"}} fit="contain"
+               alt="Title Image" asChild>
+          <NextImage src={ticket} alt={"Referral link image"}/>
+        </Image>
+      </HStack>
+    )
+  }
   const BuySection = () => {
-    const [amountToSend, setAmountToSend] = useState([1])
+    const [tickets, setTickets] = useState([1])
+    const notworkAmount = tickets[0] * 1000
     return (
       <VStack alignItems="center" gap={"2rem"}>
-        <VStack alignItems="center" w={"100%"} gap={"0.3rem"}>
-          <HStack alignItems="center">
-            <Heading textAlign="center" textStyle={{base: "4xl", lg: "6xl"}}>
-              {amountToSend}
-            </Heading>
-            <Image w="10rem" h="10rem" fit="contain" alt="Title Image" asChild>
-              <NextImage src={ticket} alt={"Referral link image"}/>
-            </Image>
-          </HStack>
-          <Slider
-            width="full"
-            variant="outline"
-            size="lg"
-            value={amountToSend}
-            onValueChange={(e) => setAmountToSend(e.value)}
-          />
-          <Text>{amountToSend} Tickets = {getNotworkAmount(amountToSend[0])} $notwork</Text>
+        <VStack alignItems="center" w={"100%"}>
+          <TicketIndicator tickets={tickets[0]}/>
+          <VStack marginTop={{base: "-2rem", md: "-1rem", lg: "-2rem"}} w={"100%"}>
+            <Slider
+              width="full"
+              variant="outline"
+              size="lg"
+              value={tickets}
+              onValueChange={(e) => setTickets(e.value)}
+            />
+            <Text>{tickets} Tickets = {notworkAmount} $notwork</Text>
+          </VStack>
         </VStack>
         <BuyButton></BuyButton>
       </VStack>
@@ -110,50 +124,58 @@ export default function Raffle() {
   }
 
   const items = [
-    {id: 1, name: "Laptop", category: "Electronics", price: 999.99},
-    {id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99},
-    {id: 3, name: "Desk Chair", category: "Furniture", price: 150.0},
-    {id: 4, name: "Smartphone", category: "Electronics", price: 799.99},
-    {id: 5, name: "Headphones", category: "Accessories", price: 199.99},
+    {id: 1, tickets: "Laptop", address: "Electronics"},
+    {id: 2, tickets: "Coffee Maker", address: "Home Appliances"},
+    {id: 3, tickets: "Desk Chair", address: "Furniture"},
   ]
 
   const LeaderBoard = () => {
     return (
-      <Table.Root size="sm" interactive>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>Product</Table.ColumnHeader>
-            <Table.ColumnHeader>Category</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {items.map((item) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>{item.category}</Table.Cell>
-              <Table.Cell textAlign="end">{item.price}</Table.Cell>
+      <VStack w={"100%"} gap={"1rem"} marginBottom={"2rem"}>
+        <Heading size={{base: "4xl", md: "5xl"}} className={styles.goldEffect}>Leaderboard</Heading>
+        <Table.Root variant={"outline"} size="lg" w={"75%"} className="dark">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Tickets</Table.ColumnHeader>
+              <Table.ColumnHeader>Address</Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {items.map((item) => (
+              <Table.Row key={item.id}>
+                <Table.Cell>{item.tickets}</Table.Cell>
+                <Table.Cell>{item.address}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </VStack>
     )
   }
 
 
   return (
     <VStack>
-      <Image h="12rem" fit="contain" alt="Title Image" asChild>
+      <Image marginTop="1rem" h={{base: "8rem", xl: "12rem"}} fit="contain" alt="Title Image" asChild>
         <NextImage src={raffle_title_image} alt={"Referral link image"}/>
       </Image>
-      <Heading>{counter}</Heading>
-      <HStack alignItems={"center"} gap={"4rem"}>
-        <Image w="60%" fit="contain" alt="Title Image" asChild>
-          <NextImage src={prices} alt={"Referral link image"}/>
-        </Image>
-        <BuySection></BuySection>
-      </HStack>
-      <LeaderBoard></LeaderBoard>
+      <Heading
+        textStyle={{base: "lg", xl: "3xl"}}
+        marginTop="-2.5rem"
+        textAlign={"center"}
+        className={styles.goldEffect}
+      >
+        Raffle ends in {new Date().toString()}
+      </Heading>
+      <VStack w="100%" gap={"4rem"}>
+        <Stack direction={{base: "column", md: "row"}} w="80%" alignItems={"center"} gap={"4rem"}>
+          <Image w={{base: "80%", md: "60%"}} fit="contain" alt="Title Image" asChild>
+            <NextImage src={prices} alt={"Referral link image"}/>
+          </Image>
+          <BuySection></BuySection>
+        </Stack>
+        <LeaderBoard></LeaderBoard>
+      </VStack>
     </VStack>
   )
 }
